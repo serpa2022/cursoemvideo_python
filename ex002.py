73b83827336ba8bdc3af9c895f213d220c120281
Felippe Serpa <107638261+serpa2022@users.noreply.github.com>
2025-06-19 09:23:29 -0300

    Atualizar o ex002.py
    Inserção de comentários ao código

--- ex002.py
+++ ex002.py
@@ -1,2 +1,7 @@
+#Programa para ler o nome e cumprimentar a pessoa
+
+#Lendo o nome
 nome = input ('Qual é seu nome?')
+
+#Escrevendo a mensagem de cumprimento
 print ("Prazer em te conhecer, ", nome, "!")
